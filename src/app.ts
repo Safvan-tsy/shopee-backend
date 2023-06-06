e26e0b9e3a42980a8bd9b3a1140140aef3fa62d0
Safvan-tsy <safvanmanikulath@gmail.com>
2023-06-06 21:45:37 +0530

    initial setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
-import express from 'express';
+import express, { Express } from 'express';
+import cookieParser from 'cookie-parser';
+import { Request, Response, NextFunction } from 'express';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
 
+import AppError from './utils/appError';
+import errorHandler from './controllers/errorController';
+import userRouter from './routes/userRouter';
 
-const app = express();
+const app: Express = express();
 
+app.use(helmet());
+app.use(express.json({ limit: '2mb' }));
+app.use(express.urlencoded({ extended: true, limit: '2mb' }));
+app.use(cookieParser());
+app.use(mongoSanitize());
 
+app.use('/api/user',userRouter)
 
-module.exports = app;
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
+})
+
+app.use(errorHandler);
+
+export default app;
