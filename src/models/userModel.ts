35ebe7e2781adb09e5dbfdc3b694c78b55944d50
Safvan-tsy <safvanmanikulath@gmail.com>
2023-06-08 21:42:32 +0530

    update user schema

---
+++ src/models/userModel.ts
@@ -0,0 +1,41 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface Users extends Document {
+    name: string;
+    image: string;
+    email: string;
+    password: string;
+    isAdmin: string;
+}
+
+const usersSchema: Schema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+
+
+const User = mongoose.model<Users>('Users', usersSchema);
+
+export default User
