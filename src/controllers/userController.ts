0083efe6c5bbfc190ec465f1f3c0e8da86fb7604
Safvan-tsy <safvanmanikulath@gmail.com>
2023-06-16 22:53:23 +0530

    fix: error handler

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,24 +4,24 @@ import { Request, Response, NextFunction } from "express";
 import User from "../models/userModel";
 
 export const registerUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const {name, email} = req.body;
+    const { name, email } = req.body;
 })
 
 export const authUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-const {email, password} = req.body;
-console.log('hi')
+    const { email, password } = req.body;
 
-if (!email || !password) {
-    return next(new AppError('Please provide email and password!', 400)); ////problem in App Error , not throwing error
-  }
-  console.log('hi 2')
-const user = await User.findOne({email}).select('+password');
-    
-if (!user || !(await user.correctPasswords(password, user.password))) {
-    console.log('hi 3')
-  return next(new AppError('Incorrect Email or password!', 404));
-}
-console.log('hi 4')
+
+    if (!email || !password) {
+        return next(new AppError('Please provide email and password!', 400)); ////problem in App Error , not throwing error
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPasswords(password, user.password))) {
+
+        return next(new AppError('Incorrect Email or password!', 404));
+    }
+
 })
 
 export const logoutUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
