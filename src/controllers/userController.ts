e114b8a896b646ccd2f9707f95fec37505514258
Safvan-tsy <safvanmanikulath@gmail.com>
2023-06-17 13:12:52 +0530

    jwt token creation

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -15,9 +15,9 @@ export const createSendToken = (user:any,statusCode:number, res:Response ) => {
     const cookieOptions = {
         expires: new Date(Date.now() + Number(process.env.JWT_COOKIE_EXPIRY) * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        secure: true
+        secure: process.env.NODE_ENV !== 'development'
     };
-    if (process.env.NODE_ENV === 'development') cookieOptions.secure = false;
+
     res.cookie('jwt',token,cookieOptions)
 
     user.password = undefined;
