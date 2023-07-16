f20ccaebd524135759aa6301095922dee466d717
Safvan-tsy <safvanmanikulath@gmail.com>
2023-07-16 21:27:02 +0530

    image upload

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import app from './src/app'
+import {v2 as cloudinary} from 'cloudinary'
 
 process.on('uncaughtException', (err:Error) => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
@@ -15,7 +16,11 @@ mongoose.connect(DB, {
   dbName: process.env.DB_NAME,
 }).then(() => console.log("DB Connection Success"))
 
-
+cloudinary.config({
+  cloud_name: "unfoldcloud",
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 const port:number = Number(process.env.PORT) || 4000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
--- src/routes/productRouter.ts
+++ src/routes/productRouter.ts
@@ -1,6 +1,13 @@
 import express from 'express';
 import { protect, isAdmin } from '../controllers/authController';
-import { getAllProducts, getOneProduct, addProduct, updateProduct } from '../controllers/productsController';
+import {
+    getAllProducts,
+    getOneProduct,
+    addProduct,
+    updateProduct,
+    prodImageUploader,
+    uploadProdImages
+} from '../controllers/productsController';
 
 const router = express.Router()
 
@@ -8,6 +15,10 @@ router
     .route('/')
     .get(getAllProducts)
     .post(protect, isAdmin, addProduct)
+
+router
+    .route('/image')
+    .post(protect, isAdmin, uploadProdImages, prodImageUploader)
 
 router
     .route('/:id')
