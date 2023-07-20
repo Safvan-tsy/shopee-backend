8b71f0cb3a70e46d83a7657035899d386e27e3ea
Safvan-tsy <safvanmanikulath@gmail.com>
2023-07-20 22:36:56 +0530

    fixes

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -40,9 +40,9 @@ declare global {
 }
 
 const  getAllProducts = catchAsync(async (req, res, next) => {
-    const limit = Number(req.query.limit)||10;
+    const limit = 2;
     const page = Number(req.query.page)||1;
-    const products = await Product.find().limit(limit).skip(page * (page-1));
+    const products = await Product.find().limit(limit).skip(limit * (page-1));
     const count = await Product.countDocuments();
 
     res.status(200).json({
